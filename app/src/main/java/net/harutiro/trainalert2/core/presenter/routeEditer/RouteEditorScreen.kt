9188e3e1ab37415c53dec3887061dc9420713d90
nekonekocatcat <n0k2s0m1+github@gmail.com>
2024-10-12 23:09:57 +0900

    入力フォームを作りました。 ・ルート名 ・出発地点の緯度経度 ・到着地点の緯度経度 が入力できるようにしています。 保存ボタンがありますが、機能はまだつけていないのでボタンがあるだけです。

--- app/src/main/java/net/harutiro/trainalert2/core/presenter/routeEditer/RouteEditorScreen.kt
+++ app/src/main/java/net/harutiro/trainalert2/core/presenter/routeEditer/RouteEditorScreen.kt
@@ -1,17 +1,25 @@
 package net.harutiro.trainalert2.core.presenter.routeEditer
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun RouteEditScreen(
     toBackScreen: () -> Unit,
     viewModel: RouteEditorViewModel = viewModel()
-){
-    Column {
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
         Text(text = "RouteEditScreen")
 
         Button(
@@ -20,6 +28,53 @@ fun RouteEditScreen(
             }
         ) {
             Text(text = "Logのテスト")
+        }
+
+        Text(text = "入力フォーム")
+
+        // ルート名入力フィールド
+        TextField(
+            value = viewModel.title,
+            onValueChange = { viewModel.title = it },
+            label = { Text("ルート名") }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+        // 出発地点の経度入力フィールド
+        TextField(
+            value = viewModel.startLongitude,
+            onValueChange = { viewModel.startLongitude = it },
+            label = { Text("出発地点の経度") }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+        // 出発地点の緯度入力フィールド
+        TextField(
+            value = viewModel.startLatitude,
+            onValueChange = { viewModel.startLatitude = it },
+            label = { Text("出発地点の緯度") }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+        // 到着地点の経度入力フィールド
+        TextField(
+            value = viewModel.endLongitude,
+            onValueChange = { viewModel.endLongitude = it },
+            label = { Text("到着地点の経度") }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+        // 到着地点の緯度入力フィールド
+        TextField(
+            value = viewModel.endLatitude,
+            onValueChange = { viewModel.endLatitude = it },
+            label = { Text("到着地点の緯度") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "保存")
+            //保存ボタンは一旦ボタンがあるだけです。後から保存機能を追加します。
         }
 
         Button(
--- app/src/main/java/net/harutiro/trainalert2/core/presenter/routeEditer/RouteEditorViewModel.kt
+++ app/src/main/java/net/harutiro/trainalert2/core/presenter/routeEditer/RouteEditorViewModel.kt
@@ -1,6 +1,9 @@
 package net.harutiro.trainalert2.core.presenter.routeEditer
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class RouteEditorViewModel: ViewModel() {
@@ -10,5 +13,14 @@ class RouteEditorViewModel: ViewModel() {
     fun test(){
         Log.d(TAG,"経路編集画面のViewModel")
     }
+
+    var title by mutableStateOf("")
+    var startLongitude by mutableStateOf("")
+    var startLatitude by mutableStateOf("")
+    var endLongitude by mutableStateOf("")
+    var endLatitude by mutableStateOf("")
+    var isNotificationEnabled by mutableStateOf(false)
+    var isVibrationEnabled by mutableStateOf(false)
+
 
 }
