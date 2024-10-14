020e47cc27a5b45f0c9718e0936701cc93d03d52
SatohAyaka <sayaka310ss@docomo.ne.jp>
2024-10-14 12:37:41 +0900

    Update MapRepository.kt //MapEffect

--- app/src/main/java/net/harutiro/trainalert2/features/map/repository/MapRepository.kt
+++ app/src/main/java/net/harutiro/trainalert2/features/map/repository/MapRepository.kt
@@ -31,6 +31,6 @@ class MapRepository {
             }
         })
         //mapEffectsを呼び出して位置情報をカメラに入れる
-        mapEffects.MapShape(maplatitude = lastlatitude, maplongitude = lastlongitude)
+        //mapEffects.MapShape(maplatitude = lastlatitude, maplongitude = lastlongitude)
     }
 }
