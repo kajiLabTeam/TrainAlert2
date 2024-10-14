a430edc130438473c8122483cce2f77aee65d6be
SatohAyaka <x22043xx@aitech.ac.jp>
2024-10-14 13:01:00 +0900

    未使用部分の消去

--- app/src/main/java/net/harutiro/trainalert2/core/presenter/map/MapScreen.kt
+++ app/src/main/java/net/harutiro/trainalert2/core/presenter/map/MapScreen.kt
@@ -5,20 +5,15 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.android.gms.maps.model.MarkerOptions
+import net.harutiro.trainalert2.features.map.repository.MapEffects
 
 @Composable
 fun MapScreen(
     viewModel: MapViewModel = viewModel()
 ){
-//    Column {
-//        Text(text = "MapScreen")
-//        Button(
-//            onClick = {
-//                viewModel.test()
-//            }
-//        ) {
-//            Text(text = "Logのテスト")
-//        }
-//    }
+    //MapEffectの情報受け取ってGoogleMap呼び出し
+    fun mapViewTest(markerOptions: MarkerOptions){
+    }
 
 }
