007ec2c6d603bb24367141a589e764ccabb89e30
SatohAyaka <x22043xx@aitech.ac.jp>
2024-10-15 16:21:17 +0900

    現在状態保存

--- app/src/main/java/net/harutiro/trainalert2/core/presenter/map/MapViewModel.kt
+++ app/src/main/java/net/harutiro/trainalert2/core/presenter/map/MapViewModel.kt
@@ -1,16 +1,36 @@
 package net.harutiro.trainalert2.core.presenter.map
 
+import android.content.Context
+import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import net.harutiro.trainalert2.features.map.api.MapApi
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import net.harutiro.trainalert2.features.map.repository.MapOptions
+import net.harutiro.trainalert2.features.map.repository.MapRepository
 
 
-class MapViewModel:ViewModel() {
+internal class MapViewModel:ComponentActivity() {
 
     val TAG = "MapViewModel"
 
-    fun test(){
-        Log.d(TAG,"Map画面のViewModel")
-    }
-    //MapScreen呼び出し
-
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        //フルスクリーン表示
+//
+//        val Context = LocalContext.current
+//        setContent {
+//            //マップ表示
+//            MapScreen(
+//                mapApi = MapApi(context = Context),
+//                mapRepository = MapRepository(),
+//                mapOptions = MapOptions()
+//            )
+//        }
+//    }
 }
