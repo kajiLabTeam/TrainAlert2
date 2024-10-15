81cd336bd87d6b68c0701b442c98a4b5261c68d9
SatohAyaka <x22043xx@aitech.ac.jp>
2024-10-15 16:15:26 +0900

    修正

--- app/src/main/java/net/harutiro/trainalert2/core/presenter/MainPage.kt
+++ app/src/main/java/net/harutiro/trainalert2/core/presenter/MainPage.kt
@@ -1,8 +1,5 @@
 package net.harutiro.trainalert2.core.presenter
 
-import android.Manifest
-import android.app.Activity
-import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -18,9 +15,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.core.app.ActivityCompat
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import net.harutiro.trainalert2.R
@@ -28,24 +23,12 @@ import net.harutiro.trainalert2.core.entities.BottomNavigationItem
 import net.harutiro.trainalert2.core.presenter.component.BottomNavigationBar
 import net.harutiro.trainalert2.core.router.BottomNavigationBarRoute
 import net.harutiro.trainalert2.core.router.MainRouter
-import net.harutiro.trainalert2.features.map.api.MapApi.MyLocationCallback
-
 
 @OptIn(
     ExperimentalMaterial3Api::class,
 )
 @Composable
 fun FirstPage(){
-
-    val context = LocalContext.current
-    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-        ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-        // 必要な場合、パーミッションを要求
-        if (context is Activity) {
-            ActivityCompat.requestPermissions(context, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 1000)
-        }
-        return
-    }
 
     var selectedItemIndex by remember { mutableIntStateOf(0) }
     val navController = rememberNavController()
