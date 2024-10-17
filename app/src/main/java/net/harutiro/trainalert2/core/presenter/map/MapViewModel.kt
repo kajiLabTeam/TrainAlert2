b43f5d311b962fcc31a961f62ced99c3f8faa0c3
SatohAyaka <x22043xx@aitech.ac.jp>
2024-10-17 13:02:39 +0900

    MapApi通さない方法の模索

--- app/src/main/java/net/harutiro/trainalert2/core/presenter/map/MapViewModel.kt
+++ app/src/main/java/net/harutiro/trainalert2/core/presenter/map/MapViewModel.kt
@@ -6,36 +6,31 @@ import androidx.lifecycle.ViewModel
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.CameraPositionState
-import net.harutiro.trainalert2.features.map.api.MapApi
 import net.harutiro.trainalert2.features.map.entity.LocationData
 import net.harutiro.trainalert2.features.map.repository.MapRepository
 
 class MapViewModel: ViewModel(){
 
-    var mapApi: MapApi? = null
     private val mapRepository = MapRepository()
 
     fun init(context: Context){
-        mapApi = MapApi(context)
+        mapRepository.initMapApi(context)
     }
 
     fun stopLocationUpdates(){
-        mapApi?.stopLocationUpdates()
+        mapRepository.stopLocationUpdates()
     }
 
     fun changeLocation(cameraPositionState: CameraPositionState) {
-        mapRepository.getMapLocationData(
-            mapApi = mapApi,
-            getLocation = { locationData ->
-                mapCameraPosition(
-                    cameraPosition = LocationData(
-                        latitude = locationData.latitude,
-                        longitude = locationData.longitude
-                    ),
-                    cameraPositionState = cameraPositionState
-                )
-            }
-        )
+        mapRepository.getMapLocationData{ locationData ->
+            mapCameraPosition(
+                cameraPosition = LocationData(
+                    latitude = locationData.latitude,
+                    longitude = locationData.longitude
+                ),
+                cameraPositionState = cameraPositionState
+            )
+        }
     }
 
     private fun mapCameraPosition(cameraPosition: LocationData, cameraPositionState: CameraPositionState) {
--- app/src/main/java/net/harutiro/trainalert2/features/map/repository/MapRepository.kt
+++ app/src/main/java/net/harutiro/trainalert2/features/map/repository/MapRepository.kt
@@ -1,13 +1,25 @@
 package net.harutiro.trainalert2.features.map.repository
 
+import android.content.ContentValues.TAG
 import android.location.Location
+import android.util.Log
 import net.harutiro.trainalert2.features.map.api.MapApi
 import net.harutiro.trainalert2.features.map.entity.LocationData
 
 //マップの中心の値の取得
 class MapRepository {
+
+    private var mapApi: MapApi? = null
+
+    fun initMapApi(context: android.content.Context) {
+        mapApi = MapApi(context)
+    }
+
+    fun stopLocationUpdates() {
+        mapApi?.stopLocationUpdates()
+    }
+
     fun getMapLocationData(
-        mapApi: MapApi?,
         getLocation:(LocationData) -> Unit
     ){
         //GoogleMap表示用（一度のみ位置情報取得）
@@ -20,10 +32,13 @@ class MapRepository {
                             longitude = location.longitude
                         )
                     )
+                }else{
+                    Log.d(TAG, "onLocationResult: null")
                 }
             }
             override fun onLocationError(error: String) {
                 // エラー処理
+                Log.d(TAG, "onLocationError")
             }
         })
     }
