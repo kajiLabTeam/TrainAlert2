461bd8d8e7e864871f865304c2d1018bbff5dedc
SatohAyaka <x22043xx@aitech.ac.jp>
2024-10-17 14:20:00 +0900

    トーストメッセージ変更

--- app/src/main/java/net/harutiro/trainalert2/features/location/api/LocationAPI.kt
+++ app/src/main/java/net/harutiro/trainalert2/features/location/api/LocationAPI.kt
@@ -72,12 +72,12 @@ class LocationAPI(private val context: Context) {
                         callback.onLocationResult(location)
                     } else {
                         callback.onLocationError("Location is null")
-                        Toast.makeText(context, "Location is null", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(context, "R.string.Location_is_null", Toast.LENGTH_SHORT).show()
                     }
                 }
                 .addOnFailureListener { exception ->
                     callback.onLocationError(exception.message ?: "Unknown error")
-                    Toast.makeText(context, "Unknown error", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, "R.string.Unknown_error", Toast.LENGTH_SHORT).show()
                 }
         }else{
             ActivityCompat.requestPermissions(context as Activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 1000)
