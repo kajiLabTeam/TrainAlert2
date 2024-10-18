a7e48b47d13cb5001421d03ef7048fae6a5683a3
SatohAyaka <x22043xx@aitech.ac.jp>
2024-10-18 17:23:39 +0900

    通知判定用関数の作成

---
+++ app/src/main/java/net/harutiro/trainalert2/features/sendnotification/repository/sendNotification.kt
@@ -0,0 +1,12 @@
+package net.harutiro.trainalert2.features.sendnotification.repository
+
+import net.harutiro.trainalert2.features.location.api.LocationAPI
+import net.harutiro.trainalert2.features.location.repository.DistanceJudgement
+import net.harutiro.trainalert2.features.room.AppDatabase
+
+class sendNotification {
+    fun notificationFunction(locationAPI: LocationAPI, distanceJudgement: DistanceJudgement, appDatabase: AppDatabase){
+//        val distanceJudgement = DistanceJudgement()
+//        val locationRepository = LocationRepository()
+    }
+}
