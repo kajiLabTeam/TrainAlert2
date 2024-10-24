905c0178d42d282e96e48603ac3ab97533007471
harutiro <harutiro2027@outlook.jp>
2024-10-24 14:42:29 +0900

    fix: いらない関数の消去い。

--- app/src/main/java/net/harutiro/trainalert2/core/presenter/home/HomeViewModel.kt
+++ app/src/main/java/net/harutiro/trainalert2/core/presenter/home/HomeViewModel.kt
@@ -43,14 +43,4 @@ class HomeViewModel : ViewModel() {
             _routeList.value = routeRepository.loadAllRoutes()
         }
     }
-
-    // データベースからすべてのルートを取得する
-    fun getAllRoutes() {
-        // IOスレッドでデータを取得
-        viewModelScope.launch(Dispatchers.IO) {
-            val routes = routeRepository.loadAllRoutes()
-            // 必要に応じてUIスレッドにデータを反映させる処理を追加
-        }
-    }
-
 }
