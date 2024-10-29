b4b013a9c76c58c5c0b171c326fb86b10b25e4fa
nekonekocatcat <n0k2s0m1+github@gmail.com>
2024-10-29 15:46:42 +0900

    戻るボタンが押された時に「保存せずに戻りますか？」というダイアログを表示したよ

--- app/src/main/java/net/harutiro/trainalert2/core/presenter/routeEditer/RouteEditorScreen.kt
+++ app/src/main/java/net/harutiro/trainalert2/core/presenter/routeEditer/RouteEditorScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
@@ -56,6 +57,9 @@ fun RouteEditScreen(
 
     val viewModel: RouteEditorViewModel = viewModel(factory = RouteEditorViewModelFactory(homeViewModel))
     val context = LocalContext.current
+
+    // ダイアログ表示フラグ
+    var isDialogOpen by remember { mutableStateOf(false) }
 
     // ルートが渡された場合は、データをロードする
     LaunchedEffect(routeId) {
@@ -158,7 +162,7 @@ fun RouteEditScreen(
         ) {
             Button(
             onClick = {
-                toBackScreen()
+                isDialogOpen = true
             },
             modifier = Modifier.weight(1f)
             ) {
@@ -184,6 +188,26 @@ fun RouteEditScreen(
             Spacer(modifier = Modifier.width(16.dp))
 
         }
+    }
+    // ダイアログ表示
+    if (isDialogOpen) {
+        AlertDialog(
+            onDismissRequest = { isDialogOpen = false },
+            text = { Text("保存せずに戻りますか？") },
+            confirmButton = {
+                Button(onClick = {
+                    isDialogOpen = false
+                    toBackScreen() // ホーム画面に戻る処理
+                }) {
+                    Text("はい")
+                }
+            },
+            dismissButton = {
+                Button(onClick = { isDialogOpen = false }) {
+                    Text("キャンセル")
+                }
+            }
+        )
     }
 
     // ポップアップの表示
