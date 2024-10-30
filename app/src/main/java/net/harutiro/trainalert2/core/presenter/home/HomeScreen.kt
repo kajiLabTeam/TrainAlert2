c9e818ffeb02f8ae9a48465c7ed6ea5d7f5b4ccf
nekonekocatcat <n0k2s0m1+github@gmail.com>
2024-10-30 17:16:13 +0900

    使ってないところを消した

--- app/src/main/java/net/harutiro/trainalert2/core/presenter/home/HomeScreen.kt
+++ app/src/main/java/net/harutiro/trainalert2/core/presenter/home/HomeScreen.kt
@@ -1,7 +1,5 @@
 package net.harutiro.trainalert2.core.presenter.home
 
-import android.os.VibrationEffect
-import android.os.Vibrator
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,14 +13,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import net.harutiro.trainalert2.TrainAlertApplication // Ensure to import your application context
-import net.harutiro.trainalert2.features.notification.api.NotificationApi
 import net.harutiro.trainalert2.features.room.routeDB.entities.RouteEntity
 import androidx.compose.runtime.LaunchedEffect
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 
 
@@ -31,7 +25,6 @@ fun HomeScreen(
     toRouteEditor: (Int?) -> Unit,
     viewModel: HomeViewModel = viewModel() // ここで viewModel を宣言
 ) {
-    val context = LocalContext.current
     val routeList by viewModel.routeList.collectAsState(initial = emptyList())
     val (showDeleteDialog, setShowDeleteDialog) = remember { mutableStateOf<Pair<RouteEntity, Boolean>?>(null) }
 
